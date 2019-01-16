55635e6f7b5975e9d70e087df72197f895c273c0
Abimanyu H K <abimanyu.hk@cxtorg.net>
2019-01-16 18:16:53 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from distutils.core import Extension
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aws-psycopg2",
+    version="1.0.1",
+    author="Abhimanyu HK",
+    author_email="manyu1994@hotmail.com",
+    description="A aws psycopg2 package from psycopg2.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AbhimanyuHK/aws-psycopg2",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
